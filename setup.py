c1ca478d8d916a41c0ccc7a9db5fae336208b2d5
wtf-ftw <56961735+wtf-ftw@users.noreply.github.com>
2019-10-24 13:20:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wtf-ftw',
+    version='1.0',
+    install_requires=[],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    url='https://github.com/wtf-ftw/wtf-ftw',
+    description='Hello world',
+)
